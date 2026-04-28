7dee3b262f26a3ff64b2d32cec7f627370adb907
Muhammad Talha <talhaowais345@gmail.com>
2026-04-29 03:24:21 +0500

    portfolio setup done

---
+++ my-website/src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./sections/Hero";
+
+function App() {
+  return (
+    <div className="bg-black text-white">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-website/src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-center">
+        I used to test software…
+        <br />
+        Now I build it.
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
